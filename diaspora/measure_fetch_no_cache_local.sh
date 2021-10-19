742225b700e22cda641221792020bd521fcb68da
Wen Zhang <zhangwen@cs.berkeley.edu>
2021-10-19 06:51:22 +0000

    fix

--- diaspora/measure_fetch_no_cache_local.sh
+++ diaspora/measure_fetch_no_cache_local.sh
@@ -7,10 +7,10 @@ if grep -q 'enable_caching=true' "$HOME/diaspora/.rbenv-vars"; then
   exit 1
 fi
 
-for i in `seq 1 2`; do
+for i in `seq 1 5`; do
   ~/start_diaspora.sh production_mod_checked &
   sleep 40
   numactl -N 1 -m 1 python3 diaspora/measure_fetch.py "no-cache$i" --warmup-rounds "$rounds" --measure-rounds "$rounds" >> "$output_dir/no-cache$i.csv"
 done
 
-# sudo shutdown -h now
+sudo shutdown -h now
