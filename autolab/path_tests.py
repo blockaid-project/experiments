06ae6fead81ea34897f9eeb50e79b6803b8bbc7d
Wen Zhang <zhangwen@cs.berkeley.edu>
2021-12-11 15:59:32 -0800

    autolab plt

---
+++ autolab/path_tests.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from common.measure_fetch import PathTest, JSON_HEADERS
+
+from constants import STUDENT_COOKIES, INSTRUCTOR_COOKIES
+
+
+DEFAULT_SLEEP = {"original": .2, "modified": .2, "cached": .2, "no-cache": 0}
+PATH_TESTS: List[PathTest] = [
+    PathTest("/", cookies=STUDENT_COOKIES, content_snippet="Course2 (SEM)", is_page_main=True, tag2sleep_s=DEFAULT_SLEEP),
+
+    PathTest("/courses/Course0/assessments", cookies=STUDENT_COOKIES, content_snippet="Quiz 4", is_page_main=True, tag2sleep_s={"original": .3, "modified": .3, "cached": .3, "no-cache": 0}),
+    PathTest("/courses/Course0/metrics/get_num_pending_instances", cookies=STUDENT_COOKIES, headers=JSON_HEADERS, status_code=403),
+
+    PathTest("/courses/Course0/assessments/quiz4", cookies=STUDENT_COOKIES, content_snippet="user0@foo.bar_0_handin.c", is_page_main=True, tag2sleep_s=DEFAULT_SLEEP),
+
+    PathTest("/courses/Course0/assessments/quiz4/submissions/26000028/download", cookies=STUDENT_COOKIES, content_snippet="printf", is_page_main=True, tag2sleep_s=DEFAULT_SLEEP),
+
+    PathTest("/courses/Course0/assessments/quiz4/viewGradesheet", cookies=INSTRUCTOR_COOKIES, content_snippet="user40@foo.bar", is_page_main=True, tag2sleep_s=DEFAULT_SLEEP)
+]
