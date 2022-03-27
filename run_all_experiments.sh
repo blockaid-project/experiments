9a1a0bb448204f862176c1456b499c3a14cbc8b9
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-03-26 18:18:45 -0700

    check web server running, etc.

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -58,8 +58,8 @@ do
             exit 0
         fi
 
-        exec > >(tee -ia "$output_dir/stdout.log")
-        exec 2> >(tee -ia "$output_dir/stderr.log")
+        exec > >(tee -i "$output_dir/stdout.log")
+        exec 2> >(tee -i "$output_dir/stderr.log")
 
         cache_config_path="$SCRIPT_DIR/common/$cache_config.rbenv-vars"
         cp "$cache_config_path" "$app_path/.rbenv-vars"
@@ -73,6 +73,14 @@ do
         "$experiment_config_path/start_server.sh" "$env" "$app_path" "$output_dir/server.log" &
         echo "Waiting $SLEEP_S seconds for web server to start up..."
         sleep "$SLEEP_S"
+
+        # TODO(zhangwen): look for the web server process directly using its PID file.
+        if ! pgrep -x "java" > /dev/null
+        then
+          echo "FATAL: Web server is not running."
+          cat "$output_dir/server.log"
+          exit 1
+        fi
 
         numactl -N 1 -m 1 "$SCRIPT_DIR/measure_latency.py" "$measure_kind" "$tag" "$experiment_config_path/tests.yaml" \
         --warmup-rounds="$rounds" --measure-rounds="$rounds" > "$output_dir/data.csv.tmp"
