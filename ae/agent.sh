34e561a1d18496cb64ccd2f05584192f80317ae9
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-04-12 19:12:18 -0700

    args fix

--- ae/agent.sh
+++ ae/agent.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+args=("$@")
+
 source "$HOME/.profile"
 workon experiments
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-"$SCRIPT_DIR/../run_experiment.sh" "$@"
+"$SCRIPT_DIR/../run_experiment.sh" "${args[@]}"
